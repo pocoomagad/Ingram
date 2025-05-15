772fa5abd0a6149a03c6fc58bc2e417b17fb9688
pocoomagad <kalaevkirill32@gmail.com>
2025-05-15 15:54:07 +0300

    architecture of project

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    description="Analogie of Instagram with chats on websokets", 
+    title="INGRAM",
+    version="0.1.0",
+    summary="New explore with websokets",
+    )
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
